039da0623b468962429c798d980afde90d6c6e96
HeyArtem <artem_white@mail.ru>
2024-07-04 00:18:14 +0300

    Добавил фу-ю регистрации пользователей (шаблон, форму, представление, путь, валидацию пароля и дублирования майла)

--- users/forms.py
+++ users/forms.py
@@ -26,3 +26,45 @@ class LoginUserForm(AuthenticationForm):
         '''
         model = get_user_model()
         fields = ['username', 'password']
+
+class RegisterUserForm(forms.ModelForm):
+    '''
+        Форма д\регистрации пользователя (для registe.html)
+        forms.ModelForm (т.к. при рес-ции поль-ля, будет добавляться новЗапись в БД, форма должна быть связана с моделью)
+        инфа по полям: https://docs.djangoproject.com/en/4.2/topics/auth/default/
+    '''
+    username = forms.CharField(label='Логин')
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput())
+
+    class Meta:
+        '''
+            class Meta - т.к. эта форма связана с моделью
+            labels - метки д\полей
+        '''
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+        }
+
+    def clean_password2(self):
+        '''
+            Валидация, проверка совпадения введеных паролей.
+        '''
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password']
+
+    def clean_email(self):
+        '''
+            Валидация, проверка, есть ли в БД пользователь с таким майлом
+        '''
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError('Такой E-mail уже существует!')
+        return email
+
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, HttpResponse
 from django.urls import reverse, reverse_lazy
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -25,28 +25,29 @@ class LoginUser(LoginView):
     #     return reverse_lazy('home')
 
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         # В таком случае форму заполняю данными из request.POST
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             # Аутентификация Проверка, что пользователь есть в БД
-#             user = authenticate(request, username=cd['username'],
-#                                 password=cd['password'])
-#             # Проверка, что oн активный
-#             if user and user.is_active:
-#                 # Авторизирую
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
-#     # Если метод GET
-#     else:
-#         form = LoginUserForm()
-#
-#     return render(request, 'users/login.html', {'form': form})
-
-
-# def logout_user(request):
-#     logout(request)
-#     # потому что пространство имен (namespace='users') переводит нас на приложение users
-#     return HttpResponseRedirect(reverse('users:login'))
+def register(request):
+    '''
+        Фу-я представления для регистрации пользователя
+    '''
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            '''
+                Если метод передачи POST, 
+                создаю форму с данными, котор были переданы,
+                проверяю, что все поля заполнены верно,
+                создаю объект user, но не заношу в БД,
+                шифрую пароль (метод set_password) и заношу его в атрибут 'password',
+                записываю в БД
+             
+            '''
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        '''
+        Если GET-запрос, формирую пустую форму и отображаю на register.html
+        '''
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
